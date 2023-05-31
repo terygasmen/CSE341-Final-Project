6861fca9a51154b35b33bb12aa4b560934ca225d
terygasmen <terygasmen@gmail.com>
2023-05-31 14:12:17 +0800

    created routes for item collection

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/restaurants', require('./restaurants'));
 router.use('/menu',require('./menu'));
 router.use('/users', require('./users'));
+router.use('/items', require('./items'));
 
 module.exports = router;
